8479f074aaa9696280850428279e79f8b7015a79
Gold <pnurep@gmail.com>
2017-03-29 17:57:05 +0900

    Make Action Bar Title, Up button

--- app/src/main/java/com/ahchim/android/ritto/FullScannerFragmentActivity.java
+++ app/src/main/java/com/ahchim/android/ritto/FullScannerFragmentActivity.java
@@ -1,13 +1,21 @@
 package com.ahchim.android.ritto;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FullScannerFragmentActivity extends AppCompatActivity {
+public class FullScannerFragmentActivity extends BaseScannerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("QR Code Reader");
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         setContentView(R.layout.activity_full_scanner_fragment);
+        //setupToolbar();
     }
 }
